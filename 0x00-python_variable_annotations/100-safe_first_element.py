ebfbc59ea40c025b359ae63b5778749ab51d170f
karim-moharm <karimmoharm222@gmail.com>
2024-03-15 03:43:36 +0200

    duck type task2

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,7 @@ from typing import Sequence, Any, Union
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """return first element ot None if list are not exist"""
     if lst:
         return lst[0]
     else:
